02cecd93fa91820f8b067353d12fc565fb24e81c
brothersw <tylerofawesomeness@gmail.com>
2023-07-26 13:35:12 -0400

    importing 364 Base Falcon Swerve

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.statemachine.Controllers;
 import frc.robot.statemachine.RobotStateManager;
 import frc.robot.states.AutoState;
@@ -15,6 +16,12 @@ import frc.robot.states.TestState;
 public class Robot extends TimedRobot {
   private Controllers controllers;
   private RobotStateManager stateMachine;
+
+  public static CTREConfigs ctreConfigs;
+
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
 
   @Override
   public void robotInit() {
@@ -27,6 +34,10 @@ public class Robot extends TimedRobot {
       new TeleOpState("teleOp", null),
       new TestState("test", null)
     );
+
+    ctreConfigs = new CTREConfigs();
+    // TODO: remove robot container and commands
+    m_robotContainer = new RobotContainer();
   }
 
   @Override
@@ -48,6 +59,14 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     stateMachine.setState("auto");
+
+    //TODO: remove auto commands
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   @Override
